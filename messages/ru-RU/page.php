c34f6a1e5fe67fccb76b2605596357c66768f3c9
Dmitry Khlystov <dkhlystov@gmail.com>
2016-07-18 02:07:52 +1000

    Translation added

--- messages/ru-RU/page.php
+++ messages/ru-RU/page.php
@@ -1,4 +1,20 @@
 <?php
 
 return [
+	'Pages' => 'Страницы',
+	'Create page' => 'Добавить страницу',
+
+	'Changes saved successfully.' => 'Изменения успешно сохранены.',
+	'Page not found.' => 'Страница не найдена.',
+	'Page deleted successfully.' => 'Страница успешно удалена.',
+	'Error occurred while image uploading.' => 'Во время загрузки изображения произошла ошибка.',
+
+	'Alias' => 'ЧПУ',
+	'Title' => 'Заголовок',
+	'Active' => 'Активный',
+	'Content' => 'Текст',
+
+	'Save' => 'Сохранить',
+	'Cancel' => 'Отмена',
+	'Create' => 'Добавить',
 ];
